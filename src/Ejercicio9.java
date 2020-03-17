f3c3ca22abd823764e3a3e20f1dd88e0d621995b
Miguel Guadalupe Ledesma Duarte <miguel.gledesma.duarte@gmail.com>
2020-03-17 08:04:49 -0600

    Conversiones realizadas de yardas, pulgadas y centimetros

--- src/Ejercicio9.java
+++ src/Ejercicio9.java
@@ -1,4 +1,3 @@
-
 /** TODO: 
  *       Elabore un algoritmo que lea un número de pies y calcule e imprima
  * su equivalente en yardas, pulgadas, centímetros y metros, de acuerdo
@@ -16,31 +15,47 @@ public class Ejercicio9 {
     public static void main(String[] args) {
 
         // #region Varibales
-        final double metro = 100.0;
-        final double cMtro = 1.0;
-        final double pulgada = 2.54;
+        double inPie    = 0.0;
+        double yardas   = 0.0;
+        
+        double cMtro = 1.0;
+        final double metro = 100;
+        double pulgada = 2.54;
 
         // Conversiones ya realizadas.
-        final double pie = 12 * pulgada;
-        final double yarda = 3 * pie;
-
-        double inPie = 0.0;
+        double pie = 12 * pulgada;
         Scanner input = new Scanner(System.in);// Entrada de datos.
         // #endregion
 
-        System.out.println("You can't reboot the program without calculating the neural ADP firewall!\n");
-        System.out.print(".. Introduzca la cantidad de pies que necesota convertir#-->:");
-        do {
+        System.out.println("\n You can't reboot the program without calculating the neural ADP firewall!\n");
+        do  {
+            
+            System.out.print("<|°_°|>  .. Introduzca la cantidad de pies que necesota convertir#-->:");
             try {
                 inPie = input.nextDouble();
-                
-            } catch (InputMismatchException ex1) {
-                    System.out.print("¡¡Introduzca un valir valido!!");
-                    System.out.print("\n :" + ex1.toString());
-                    }catch (Exception e) {
-                        //TODO: handle exception
-                        System.out.println(e.toString());
-                    }
+                //Conversiones apartir de datos proporsionados por el usuario
+                yardas  = 3 / inPie; //Yardas
+                pulgada = inPie   * 12; // Pulgadas
+                cMtro   = pulgada * 2.54;
+
+                System.out.println("\n El valor introducido es: " + inPie + " pies \n");
+                System.out.println("  *** Conversiones ******** \n" +
+                "     Yardas: " + yardas    + " \n " +
+                "   Pulgadas: " + pulgada   + " \n " +
+                "Centimetros: " + cMtro     + " \n " +
+                "     Metros: " + metro     + " \n " +
+                "Pulgadas: "    + pulgada   + " \n ");
+                break;
+            } 
+            catch (InputMismatchException ex1)    {
+                System.out.print("¡¡Introduzca un valor valido!! #Ej: 9.0# ");
+                System.out.print("\n :" + ex1.toString() + "\n" );
+            }   
+            catch (Exception e) {
+                //TODO: handle exception
+                System.out.println(e.toString());
+            }
+            input.next();
         }   while(true);
         
     }
