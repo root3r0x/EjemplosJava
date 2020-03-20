3414b913b6ca595ec94ec9ed2cb6c2c6c8a2c4e8
Miguel Guadalupe Ledesma Duarte <miguel.gledesma.duarte@gmail.com>
2020-03-20 05:39:43 -0600

    Se crea funcion para calcular y se comenta el código

---
+++ src/Ejercicio12.java
@@ -0,0 +1,33 @@
+/**
+ * Elaborar un algoritmo que lea la cantidad de dólares que se va a comprar y el
+ * tipo de cambio en pesos(costo de dólar en pesos). Calcular e imprimir la 
+ * cantidad que se debe pagar en pesos por la cantidad de dólares indicada.
+ */
+
+import java.util.Scanner;
+
+public class Ejercicio12  {
+
+   //#region Variables
+   public static double tipoCambio;
+   public static double dollar;
+   public static double peso;
+
+   public static Scanner input = new Scanner(System.in);
+   // #endregion
+
+   /** Método para calcular el costo.
+    * @param cambio Equivalente de pesos al dólar.
+    * @param cantDollares Cantidad de dolares que se desean adquirir.
+    * 
+   */
+   public static void CalculaPesos(double cantDollares, double cantPesos, double cambio) throws Exception {
+      
+      tipoCambio = cambio;
+
+   }
+   public static void main(String[] args) {
+
+      System.out.println("Hola 12 ");
+   }
+}
