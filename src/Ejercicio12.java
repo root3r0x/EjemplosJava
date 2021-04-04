73311dd92cb7429b8fe232b94254089036b92b83
Miguel Guadalupe Ledesma Duarte <miguel.gledesma.duarte@gmail.com>
2021-04-04 10:45:02 -0600

    Un comit no se que

--- src/Ejercicio12.java
+++ src/Ejercicio12.java
@@ -21,20 +21,29 @@ public class Ejercicio12 {
     * @param cantDollares Cantidad de dolares que se desean adquirir.
     * 
     */
-   public static void CalculaDolares( double cantDollares, double cambio) throws Exception {
+   public static double CalculaDolares( double cantDollares, double cambio) throws Exception {
       tipoCambio = cambio; // set tipo de cambio ingresado.
       pesos = tipoCambio * cantDollares;
+      return pesos;
    }
    public static void main(String[] args) {
      /** do {
          System.out.printf("");
          break;
       } while (true); **/
-      System.out.printf("\n _-***** Conversion de dolares a pesos *****-_ \n");
-      System.out.print("\n .-# Ingrese el tipo de cambio del día: ");
-      tipoCambio = input.nextDouble();
-      System.out.println("\n --- Tipo de cambio: $" + tipoCambio +" ---");
-      System.out.println("\nIngrese la cantidad de dolares a adquirir: ");
+      try{
+            System.out.printf("\n _-***** Conversion de dolares a pesos *****-_ \n");
+            System.out.print("\n .-# Ingrese el tipo de cambio del día: ");
+            tipoCambio = input.nextDouble();
+            System.out.println("\n --- Tipo de cambio: $" + tipoCambio +" ---");
+            System.out.print("\nIngrese la cantidad de dolares a adquirir: ");
+            cDolares = input.nextDouble();
+            System.out.println("Cantidad de dolares: $" + cDolares+ " dolares a cambiar.");
+            pesos = CalculaDolares(cDolares, tipoCambio);
+            System.out.println("\n Total a pagar en mxn es de : $" + pesos + " pesos.");
+      }catch(Exception ex){
+         System.out.println("Error: " +  ex.getMessage());
+      }
       input.close();
    }
 }
