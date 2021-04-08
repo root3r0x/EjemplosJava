2afb7356f49ef2d93a20262b5784883d7812f817
Miguel Guadalupe Ledesma Duarte <miguel.gledesma.duarte@gmail.com>
2021-04-07 19:17:31 -0600

    Se agrega el ejercicio #13

---
+++ src/Ejercicio13.java
@@ -0,0 +1,18 @@
+/** Elaborar un algoritmo que permita leer los valores: X, Y, Z Y W
+ * e imprima el valor de F.
+ * 
+ *    Formula: F = (4x^2 y^2 zw)^2 
+ *               -------------------
+ *                   4x^(1/2)
+ *                   ----------
+ *                    b^(3/4)
+ */
+
+import java.util.*;
+
+public class Ejercicio13 {
+   public static void main(String[] args)
+   {
+      System.out.println("Hola mundo");
+   }
+}
