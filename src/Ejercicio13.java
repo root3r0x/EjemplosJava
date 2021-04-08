91abbaafd33e216cbe30281a1332caa9e61829f5
Miguel Guadalupe Ledesma Duarte <miguel.gledesma.duarte@gmail.com>
2021-04-08 09:49:21 -0600

    Se crean variables, y metodos.

--- src/Ejercicio13.java
+++ src/Ejercicio13.java
@@ -7,12 +7,37 @@
  *                   ----------
  *                    b^(3/4)
  */
-
 import java.util.*;
 
 public class Ejercicio13 {
+   
+   //#region Variables
+   public static double X=0, Y=0, Z=0, W=0; //variables to read.
+   public static double b =0, F=0; //variable we wish to calculr
+   Scanner input = new Scanner(System.in);   //variable for data input.
+   //#endregion
+   public static double solveProcedure(double x,  double y, double z, double w)
+   throws Exception {
+      //variables defined by user
+      X = x; Y = y; Z = z; W = w;
+      try {
+         //formula
+         F = (4 * Math.pow(X,2) * Math.pow(Y, 2) );
+         F = Math.pow(F, 2);
+         double firtBase = 4* Math.pow(x,1/2);
+         double  base = firtBase / Math.pow(b,3/4);
+      }  catch (Exception ex) {
+         System.out.println(ex.getMessage().toString());
+      }
+      //answer
+      return F;
+   }
+   public static void Menu(){
+
+      System.out.println("\n Hola mundo");
+   }
    public static void main(String[] args)
    {
-      System.out.println("Hola mundo");
+      Menu();
    }
 }
