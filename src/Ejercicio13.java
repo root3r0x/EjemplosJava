64229891730e9bd66abcef293759d0dcee0b95e9
Miguel Guadalupe Ledesma Duarte <miguel.gledesma.duarte@gmail.com>
2021-04-11 12:05:34 -0600

    Java Doc agregado para pruebas.

--- src/Ejercicio13.java
+++ src/Ejercicio13.java
@@ -7,30 +7,41 @@
  *                   ----------
  *                    b^(3/4)
  */
+import java.lang.annotation.Documented;
 import java.util.*;
 
 public class Ejercicio13 {
    
    //#region Variables
    public static double X=0, Y=0, Z=0, W=0; //variables to read.
-   public static double b =0, F=0; //variable we wish to calculr
+   public static double b =0, f=0; //variable we wish to calculr
    Scanner input = new Scanner(System.in);   //variable for data input.
    //#endregion
+   
+   /**
+    * Double is the value for defect. 
+    * @param x value for the variable  x
+    * @param y value for the variable  y
+    * @param z value for the variable  z
+    * @param w value for the variable  w
+    * @return
+    * @throws Exception
+    */
    public static double solveProcedure(double x,  double y, double z, double w)
    throws Exception {
       //variables defined by user
       X = x; Y = y; Z = z; W = w;
       try {
          //formula
-         F = (4 * Math.pow(X,2) * Math.pow(Y, 2) );
-         F = Math.pow(F, 2);
+         f = (4 * Math.pow(X,2) * Math.pow(Y, 2) );
+         f = Math.pow(F, 2);
          double firtBase = 4* Math.pow(x,1/2);
          double  base = firtBase / Math.pow(b,3/4);
       }  catch (Exception ex) {
          System.out.println(ex.getMessage().toString());
       }
       //answer
-      return F;
+      return f;
    }
    public static void Menu(){
 
